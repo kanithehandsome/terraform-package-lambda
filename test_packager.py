a25f3b2550b911f65fa0badfe666a4fdbd123765
Jason Felice <jason.m.felice@gmail.com>
2017-04-27 13:40:12 -0400

    Extract do()

--- test_packager.py
+++ test_packager.py
@@ -2,12 +2,21 @@ import unittest
 import packager
 import zipfile
 
+def do(input_values):
+    p = packager.Packager(input_values)
+    p.package()
+    output = p.output()
+    zf = zipfile.ZipFile(output["output_filename"], 'r')
+    zip_contents = {}
+    for name in zf.namelist():
+        zip_contents[name] = zf.read(name)
+    zf.close()
+    return {
+        "output": output,
+        "zip_contents": zip_contents
+    }
+
 class TestPackager(unittest.TestCase):
     def test_packages_a_python_script_with_no_dependencies(self):
-        p = packager.Packager({
-            "code": "test/python-simple/foo.py"
-        })
-        p.package()
-        zf = zipfile.ZipFile(p.output()["output_filename"], 'r')
-        self.assertEquals(zf.read('foo.py'), "# Hello, Python!\n")
-        zf.close()
+        result = do({"code": "test/python-simple/foo.py"})
+        self.assertEquals(result["zip_contents"]["foo.py"], "# Hello, Python!\n")
