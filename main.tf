9238ce41b3a6e5bf260d04d4ff2a84ddc895b482
Jason Felice <jason.m.felice@gmail.com>
2017-05-01 15:24:48 -0400

    Update main.tf for new parameters

--- main.tf
+++ main.tf
@@ -1,10 +1,25 @@
-variable "basename" {}
+variable "code" {
+  description = "Relative path to code file.  (e.g. 'hello.js')"
+}
+variable "extra_files" {
+  description = "List of extra files to package.  (default: [])"
+  type = "list"
+  default = []
+}
+variable "output_filename" {
+  description = "Name of zip file to write.  (default: derived from 'code' variable)"
+  default = ""
+}
 
 data "external" "lambda_packager" {
   program = [ "${path.module}/packager.py" ]
   query = {
-    basename = "${var.basename}"
+    code = "${var.code}"
+    output_filename = "${var.output_filename}"
+    extra_files = [ "${var.extra_files}" ]
   }
 }
 
+output "code" { value = "${data.external.lambda_packager.code}" }
+output "output_filename" { value = "${data.external.lambda_packager.output_filename}" }
 output "output_base64sha256" { value = "${data.external.lambda_packager.output_base64sha256}" }
